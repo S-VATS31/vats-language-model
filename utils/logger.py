f4dbf85c19428b3930504705a4e8717e92bc6cea
Shaan Vats <shaanv3121@gmail.com>
2025-12-30 14:09:08 -0800

    added logger

---
+++ utils/logger.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import sys
+
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+def setup_logger(
+    name: str,
+    log_file: str,
+    level: int = logging.INFO,
+) -> logging.Logger:
+    """Set up logging module.
+    
+    Args:
+        name (str): Name of logger.
+        log_file (str): File to be logged to.
+        level (int): Level to be logged to. Defaults to `logging.INFO`.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.propagate = False
+
+    if not logger.handlers:
+        # File handler
+        file_handler = logging.FileHandler(os.path.join(log_dir, log_file))
+        file_handler.setLevel(level)
+
+        # Console handler
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(level)
+
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
